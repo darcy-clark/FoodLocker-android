763d21b4f70fe7369d23e5d24c6c66c296cdc96b
Darcy Clark <drgclark@edu.uwaterloo.ca>
2018-07-14 14:04:20 -0400

    Created semi-functional Create New Account activity prototype

--- app/src/main/java/org/foodlocker/CreateAccount.java
+++ app/src/main/java/org/foodlocker/CreateAccount.java
@@ -1,10 +1,11 @@
 package org.foodlocker;
 
 import android.app.Activity;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.Button;
 
 public class CreateAccount extends Activity {
 
@@ -14,5 +15,35 @@ public class CreateAccount extends Activity {
         this.requestWindowFeature(Window.FEATURE_NO_TITLE);
         this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_create_account);
+
+        Button userBtn = findViewById(R.id.user_btn);
+        Button volunteerBtn = findViewById(R.id.volunteer_btn);
+        userBtn.setOnClickListener(new UserTypeButtonListener(volunteerBtn));
+        volunteerBtn.setOnClickListener(new UserTypeButtonListener(userBtn));
     }
+
+    class UserTypeButtonListener implements View.OnClickListener {
+
+        Button otherButton;
+
+        UserTypeButtonListener(Button otherButton) {
+            this.otherButton = otherButton;
+        }
+
+        @Override
+        public void onClick(View view) {
+            view.setSelected(true);
+            otherButton.setSelected(false);
+            // TODO: Add functionality (saving the button text to a variable could work, no control flow)
+        }
+    }
+
+//    class VolunteerButtonListener implements View.OnClickListener {
+//
+//        @Override
+//        public void onClick(View view) {
+//            view.setSelected(true);
+//            // TODO: Add volunteer button-specific functionality
+//        }
+//    }
 }
